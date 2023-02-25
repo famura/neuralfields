314849aa337530175cc72ef3162ad27114f1e324
famura <fabio.muratore@famura.net>
2023-02-25 14:05:39 +0100

    Added example for time series generation

---
+++ examples/time_series_generation.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import seaborn
+import torch
+
+from neuralfields import EXAMPLES_DIR, PotentialBased
+
+
+if __name__ == "__main__":
+    seaborn.set_theme()
+
+    # Configure.
+    torch.manual_seed(0)
+    num_samples = 5
+    len_time_series = 50
+    random_inputs = True  # if False, the inputs will be zero for every sample (vary via the hidden state)
+    default_hidden = False  # if True, the hidden state will be initialized with zeroes by the net (vary via the inputs)
+
+    # Load the model previously trained with the time_series_learning.py example script.
+    try:
+        model = torch.load(EXAMPLES_DIR / "model.pt")
+        assert isinstance(model, PotentialBased)
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            "There was no file called 'model.pt' found in neuralfields' example directory. Most likely, you need to "
+            "run the 'time_series_learning.py' script first."
+        )
+
+    # Use the model to generate several time series. This could either be done by providing inputs that are different
+    # along the first dimension of the tensor, or by varying the initial hidden state.
+    inputs = torch.zeros(num_samples, len_time_series, model.input_size)
+    if random_inputs:
+        inputs[:, len_time_series // 2 :, :] = (
+            torch.linspace(0, 8, len_time_series // 2)  # start and end of the linspace are arbitrary
+            .view(1, -1, 1)
+            .repeat(num_samples, 1, model.input_size)
+        )
+        inputs[:, len_time_series // 2 :, :] += torch.randn(num_samples, len_time_series // 2, model.input_size)
+    if default_hidden:
+        hidden = None
+    else:
+        hidden = torch.randn(num_samples, model.hidden_size) * 5
+    with torch.no_grad():
+        generated, _ = model(inputs, hidden)
+
+    # Plot.
+    fig, axs = plt.subplots(1, 1, figsize=(16, 9))
+    for idx_ts, gen_ts in enumerate(generated):
+        plt.plot(gen_ts.numpy(), label=f"sample {idx_ts}")
+    axs.legend(bbox_to_anchor=(0, 1.02, 1, 0.2), loc="lower left", mode="expand", borderaxespad=0, ncol=inputs.size(0))
+    plt.show()
--- examples/time_series_learning.py
+++ examples/time_series_learning.py
@@ -3,13 +3,16 @@ import numpy
 import seaborn
 import torch
 
-from neuralfields import EXAMPLES_DIR, NeuralField, SimpleNeuralField, pd_cubic
+from neuralfields import EXAMPLES_DIR, NeuralField, SimpleNeuralField, pd_capacity_21_abs
 
 
-def load_and_split_data(dataset_name: str = "monthly_sunspots", normalize: bool = False):
+def load_and_split_data(dataset_name: str, normalize: bool = False):
     # Load and move the torch.
     data = torch.from_numpy(numpy.load(EXAMPLES_DIR / f"{dataset_name}.npy"))
     data = torch.atleast_2d(data).float().contiguous()
+    if data.size(0) == 1:
+        # torch.atleast_2d() adds dimensions to the front, but we want the first dim to be the length of the series.
+        data = data.T
 
     if normalize:
         data /= max(abs(data.min()), abs(data.max()))
@@ -22,10 +25,39 @@ def load_and_split_data(dataset_name: str = "monthly_sunspots", normalize: bool 
     return data, data_trn, data_tst
 
 
-def simple_training_loop(model: torch.nn.Module, packed_inputs: torch.Tensor, packed_targets: torch.Tensor):
+def create_figure(dataset_name: str) -> plt.Figure:
+    if dataset_name not in ("monthly_sunspots", "mackey_glass"):
+        raise NotImplementedError(f"Unknown dataset {dataset_name}! Please specify the necessary parts in the script.")
+
+    fig, axs = plt.subplots(2, 1, figsize=(16, 9))
+    axs[0].plot(data_trn, label="data train")
+    axs[1].plot(data_tst, label="data test")
+    axs[1].set_xlabel("months" if dataset_name == "monthly_sunspots" else "time")
+    axs[0].set_ylabel("spot count" if dataset_name == "monthly_sunspots" else "P")
+    axs[1].set_ylabel("spot count" if dataset_name == "monthly_sunspots" else "P")
+
+    return fig
+
+
+def simple_training_loop(
+    model: torch.nn.Module,
+    packed_inputs: torch.Tensor,
+    packed_targets: torch.Tensor,
+    dataset_name: str,
+    normalized_data: bool,
+):
+    # Use a simple heuristic for the optimization hyper-parameters.
+    if dataset_name == "monthly_sunspots" and not normalized_data:  # data is in [0, 100] so we need more steps
+        num_epochs = 10001 if isinstance(model, SimpleNeuralField) else 4001
+        lr = 1e-1
+    else:
+        num_epochs = 2501 if isinstance(model, SimpleNeuralField) else 501
+        lr = 1e-2
+
     loss_fcn = torch.nn.MSELoss()
-    optim = torch.optim.Adam([{"params": model.parameters()}], lr=1e-2, eps=1e-8)
-    for idx_e in range(4001 if isinstance(model, SimpleNeuralField) else 701):
+    optim = torch.optim.Adam([{"params": model.parameters()}], lr=lr, eps=1e-8)
+
+    for idx_e in range(num_epochs + 1):
         # Reset the gradients.
         optim.zero_grad(set_to_none=True)
 
@@ -54,17 +86,16 @@ if __name__ == "__main__":
     data, data_trn, data_tst = load_and_split_data(dataset_name, normalize_data)
     dim_data = data.size(1)
 
-    # Plot.
-    fig, axs = plt.subplots(2, 1, figsize=(16, 9))
-    axs[0].plot(data_trn, label="data train")
-    axs[1].plot(data_tst, label="data test")
-    axs[1].set_xlabel("months" if dataset_name == "monthly_sunspots" else "time")
-    axs[0].set_ylabel("monthly spot count" if dataset_name == "monthly_sunspots" else "")
-    axs[1].set_ylabel("monthly spot count" if dataset_name == "monthly_sunspots" else "")
+    fig = create_figure(dataset_name)
 
     # Create the neural field.
     if use_simplification:
-        model = SimpleNeuralField(input_size=dim_data, output_size=dim_data, potentials_dyn_fcn=pd_cubic)
+        model = SimpleNeuralField(
+            input_size=dim_data,
+            output_size=dim_data,
+            potentials_dyn_fcn=pd_capacity_21_abs,
+            activation_nonlin=torch.tanh,
+        )
     else:
         model = NeuralField(input_size=dim_data, hidden_size=13, output_size=dim_data, conv_out_channels=1)
     print(f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
@@ -75,7 +106,7 @@ if __name__ == "__main__":
     # i i ... t
     inputs = []
     targets = []
-    len_window = 20
+    len_window = 10  # tested 10 and 20
     for idx in range(1, data_trn.size(0)):
         # Slice the input.
         idx_begin = max(idx - len_window, 0)
@@ -94,7 +125,7 @@ if __name__ == "__main__":
     packed_targets = torch.stack(inputs, dim=0)
 
     # Run a simple optimization loop.
-    simple_training_loop(model, packed_inputs, packed_targets)
+    simple_training_loop(model, packed_inputs, packed_targets, dataset_name, normalize_data)
 
     # Evaluate model.
     with torch.no_grad():
@@ -104,9 +135,13 @@ if __name__ == "__main__":
         predictions_tst, _ = model(data_tst[:-1].unsqueeze(0), hidden=None)
         predictions_tst = predictions_tst.squeeze(0).detach().numpy()
 
+    # Safe the model.
+    torch.save(model, EXAMPLES_DIR / "model.pt")
+
     # Plot the results.
+    axs = fig.get_axes()
     axs[0].plot(predictions_trn, label="predictions")
     axs[1].plot(predictions_tst, label="predictions")
-    axs[0].legend()
-    axs[1].legend()
+    axs[0].legend(loc="upper right", ncol=2)
+    axs[1].legend(loc="upper right", ncol=2)
     plt.show()
